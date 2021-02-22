46169a7f9822f8ebf9e5c405878d91127c51ee3d
jemerardprete <julie.emerardprete1@ipilyon.net>
2021-02-22 10:38:29 +0100

    Création méthode AugmenterSalaire() & Tests AugmenterSalaire() / GetNbRtt()

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -1,5 +1,7 @@
 package com.ipiecoles.java.java350.model;
 
+import com.ipiecoles.java.java350.exception.EmployeException;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -142,7 +144,15 @@ public class Employe {
     }
 
     //Augmenter salaire
-    //public void augmenterSalaire(double pourcentage){}
+    public Double augmenterSalaire(Double pourcentage) {
+        if(this.salaire == null) {
+            return null;
+        }
+        if(pourcentage < 0) {
+            return this.salaire;
+        }
+        return this.salaire = this.getSalaire() * (1 + pourcentage);
+    }
 
     public Long getId() {
         return id;
--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,5 +1,6 @@
 package com.ipiecoles.java.java350.model;
 
+import com.ipiecoles.java.java350.exception.EmployeException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -119,4 +120,91 @@ public class EmployeTest {
         Assertions.assertThat(prime).isEqualTo(1000);
 
     }
+
+    /* EVALUATION */
+
+    // 1. Tests Unitaires sur la méthode augmenterSalaire //
+
+    // Test Unitaire : testAugmentationSalaire
+    @Test
+    public void testAugmentationSalaire() {
+        // Given
+        Employe employe = new Employe();
+        employe.setSalaire(1500.0);
+
+        // When
+        Double salaire = employe.augmenterSalaire(0.50);
+
+        // Then
+        Assertions.assertThat(salaire).isEqualTo(2250);
+    }
+
+    // Test Unitaire : testAugmentationSalaireNull
+    @Test
+    public void testAugmentationSalaireNull() {
+        // Given
+        Employe employe = new Employe();
+        employe.setSalaire(null);
+
+        // When
+        Double salaire = employe.augmenterSalaire(0.50);
+
+        // Then
+        Assertions.assertThat(salaire).isNull();
+    }
+
+    // Test Unitaire : testAugmentation0Salaire
+    @Test
+    public void testAugmentation0Salaire() throws EmployeException {
+        // Given
+        Employe employe = new Employe();
+        employe.setSalaire(1500.0);
+
+        // When
+        Double salaire = employe.augmenterSalaire(0.00);
+
+        // Then
+        Assertions.assertThat(salaire).isEqualTo(1500.0);
+    }
+
+    // Test Unitaire : testAugmentationNegativeSalaire
+    @Test
+    public void testAugmentationNegativeSalaire() {
+        // Given
+        Employe employe = new Employe();
+        employe.setSalaire(1500.0);
+
+        // When
+        Double salaire = employe.augmenterSalaire(-0.50);
+
+        // Then
+        Assertions.assertThat(salaire).isEqualTo(1500.0);
+    }
+
+    // 2. Tests Unitaires (paramétrés) sur la méthode getNbRtt //
+
+    // Test Paramétré : Nombre RTT
+    @ParameterizedTest(name = "annee {0}, tempsPartiel {1} => nbRttAttendus {2} ")
+    @CsvSource({
+            "2019, 1.0, 8",
+            "2019, 0.5, 4",
+            "2021, 1.0, 10",
+            "2021, 0.5, 5",
+            "2022, 1.0, 10",
+            "2022, 0.5, 5",
+            "2032, 1.0, 11",
+            "2032, 0.5, 6"
+    })
+    public void testGetNbRtt(Integer annee, Double tempsPartiel, Integer nbRttAttendus) {
+        // Given
+        Employe employe = new Employe("Dupont", "Maria", "T12345", LocalDate.now().minusYears(6), 1500.0, 1, tempsPartiel);
+
+        // When
+        Integer nbRtt = employe.getNbRtt(LocalDate.of(annee, 1, 1));
+
+        // Then
+        Assertions.assertThat(nbRtt).isEqualTo(nbRttAttendus);
+
+    }
+
 }
