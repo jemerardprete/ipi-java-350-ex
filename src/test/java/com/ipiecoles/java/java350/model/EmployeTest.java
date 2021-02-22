3c384be1baf580486f3e6c2efeb0ecc76b53cbc2
jemerardprete <julie.emerardprete1@ipilyon.net>
2021-02-22 19:20:40 +0100

    Coverage : Ajout tests paramatrés testGetPrimeAnnuelle()

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -61,7 +61,10 @@ public class EmployeTest {
             "1, 'T12345', 1.0, 0, 1000.0",
             "1, 'T12345', 0.5, 0, 500.0",
             "2, 'T12345', 1.0, 0, 2300.0",
-            "1, 'T12345', 1.0, 2, 1200.0"
+            "1, 'T12345', 1.0, 2, 1200.0",
+            "1, 'M12345', 1.0, 2, 1900.0",
+            "0, 'T12345', 1.0, 2, 500.0",
+
     })
     void testGetPrimeAnnuelle(Integer performance, String matricule, Double tauxActivite, Long nbAnneesAnciennete, Double primeAttendue) {
         // Given
